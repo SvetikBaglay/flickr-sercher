9c64d1fc9ae4c3c081115c96b8b90318401fb29e
Svetlana Bahlay <conbka@gmail.com>
2021-06-06 21:39:15 +0300

    created template search page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { SearchComponent } from './search/search.component';
 import { BookmarksComponent } from './bookmarks/bookmarks.component';
@@ -14,7 +15,12 @@ import { BookmarksComponent } from './bookmarks/bookmarks.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot([
+      { path: 'search', component: SearchComponent },
+      { path: 'bookmarks', component: BookmarksComponent },
+      { path: '**', redirectTo: 'search', pathMatch: 'full'},
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
